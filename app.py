20757b08ea15dffa4dd0ae275fd9acd1ba0d3733
Renish B <renishb10@gmail.com>
2025-08-30 13:01:13 +0530

    rag qa

--- app.py
+++ app.py
@@ -1,60 +1,133 @@
+import json
+import os
+import sys
+import boto3
 import streamlit as st
-import os, time
-from dotenv import load_dotenv
-from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
+
+## We will be suing Titan Embeddings Model To generate Embedding
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+## Data Ingestion
+
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.vectorstores import FAISS
+
+# Vector Embedding And Vector Store
+
+from langchain.vectorstores import FAISS
+
+## LLm Models
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+## Bedrock Clients
+bedrock=boto3.client(service_name="bedrock-runtime")
+bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
 
 
-load_dotenv()
+## Data ingestion
+def data_ingestion():
+    loader=PyPDFDirectoryLoader("data")
+    documents=loader.load()
 
-## Load NVIDIA API KEY
-os.environ['NVIDIA_API_KEY']=os.getenv('NVIDIA_API_KEY')
+    # - in our testing Character split works better with this PDF data set
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,
+                                                 chunk_overlap=1000)
+    
+    docs=text_splitter.split_documents(documents)
+    return docs
 
-## LLM
-llm=ChatNVIDIA(model="meta/llama-3.3-70b-instruct")
+## Vector Embedding and vector store
 
-# Embedding
-def vector_embedding():
-    if "vectors" not in st.session_state:
-        st.session_state.embeddings = NVIDIAEmbeddings()
-        st.session_state.loader = PyPDFDirectoryLoader("./data")
-        st.session_state.docs = st.session_state.loader.load()
-        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50)
-        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:30])
-        st.session_state.vectors = FAISS.from_documents(documents=st.session_state.final_documents, embedding=st.session_state.embeddings)
+def get_vector_store(docs):
+    vectorstore_faiss=FAISS.from_documents(
+        docs,
+        bedrock_embeddings
+    )
+    vectorstore_faiss.save_local("faiss_index")
 
-# UI
-st.title("GoLang Q&A with NVIDIA NIM")
+def get_claude_llm():
+    ##create the Anthropic Model
+    llm=Bedrock(model_id="ai21.j2-mid-v1",client=bedrock,
+                model_kwargs={'maxTokens':512})
+    
+    return llm
 
-prompt = ChatPromptTemplate.from_template(
-    """
-    Answer the questions based on the provided context only.
-    Please provide the most accurate response based on the question
-    <context>
-    {context}
-    </context>
-    Question: {input}
-"""
+def get_llama2_llm():
+    ##create the Anthropic Model
+    llm=Bedrock(model_id="meta.llama2-70b-chat-v1",client=bedrock,
+                model_kwargs={'max_gen_len':512})
+    
+    return llm
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a 
+concise answer to the question at the end but usse atleast summarize with 
+250 words with detailed explaantions. If you don't know the answer, 
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
 )
 
-prompt1 = st.text_input("Enter your question from documents")
+def get_response_llm(llm,vectorstore_faiss,query):
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":query})
+    return answer['result']
 
-if st.button("Load Golang Brain"):
-    vector_embedding()
-    st.write("Golang NVIDIA FAISS Vector store is ready!")
 
-if prompt1:
-    document_chain = create_stuff_documents_chain(llm, prompt)
-    retriever = st.session_state.vectors.as_retriever()
-    retriever_chain = create_retrieval_chain(retriever, document_chain)
-    start = time.process_time()
-    response = retriever_chain.invoke({'input': prompt1})
-    print("Response Time : ", time.process_time() - start)
-    st.write(response['answer'])
+def main():
+    st.set_page_config("Chat PDF")
+    
+    st.header("Chat with PDF using AWS Bedrock💁")
 
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Update Or Create Vector Store:")
+        
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_claude_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+    if st.button("Llama2 Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm=get_llama2_llm()
+            
+            #faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm,faiss_index,user_question))
+            st.success("Done")
+
+if __name__ == "__main__":
+    main()
